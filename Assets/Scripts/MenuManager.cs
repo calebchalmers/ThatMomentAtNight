681dafb24ccc6df54b0852fbcab3e4ce04432b3d
Caleb Chalmers <caleb.chalmers1@gmail.com>
2022-07-27 19:24:20 -0500

    Add flashlight button selector logic

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+    [Header("Panels")]
     public GameObject mainPanel;
     public GameObject settingsPanel;
 
+    [Header("Controls")]
     public Button continueButton;
     public GameObject quitButton;
     public Slider volumeSlider;
 
+    [Header("Flashlight")]
+    public RectTransform flashlightTransform;
+    public float flashlightLerpSpeed = 60f;
+
+    [Header("Misc")]
     public AudioMixer mixer;
 
     private SceneTransition sceneTransition;
@@ -25,6 +33,34 @@ public class MenuManager : MonoBehaviour
 #if UNITY_EDITOR || UNITY_STANDALONE
         quitButton.SetActive(true);
 #endif
+    }
+
+    void Update()
+    {
+        EventSystem eventSystem = EventSystem.current;
+        PointerEventData pointerData = new PointerEventData(eventSystem);
+        pointerData.Reset();
+        pointerData.position = Input.mousePosition;
+        List<RaycastResult> result = new List<RaycastResult>();
+        eventSystem.RaycastAll(pointerData, result);
+
+        foreach (var hit in result)
+        {
+            var button = hit.gameObject.GetComponent<Button>();
+            if (button != null && button.interactable)
+            {
+                button.Select();
+            }
+        }
+
+        if (eventSystem.currentSelectedGameObject != null)
+        {
+            flashlightTransform.position = Vector3.Lerp(
+                flashlightTransform.position,
+                eventSystem.currentSelectedGameObject.transform.position,
+                flashlightLerpSpeed * Time.deltaTime
+            );
+        }
     }
 
     public void OnPressedNewGame()
